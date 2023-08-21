642df97f4c4c10a00d228e685c10c0c7c198097b
vladislav <vladisla.sv.299563@gmail.com>
2023-08-21 10:15:30 +0600

    fix playlist.py

--- src/playlist.py
+++ src/playlist.py
@@ -11,11 +11,14 @@ class PlayList:
     def __init__(self, playlist_id):
         self.playlist_videos = (PlayList.youtube.playlistItems().list
                                 (playlistId=playlist_id, part='contentDetails,snippet', maxResults=50, ).execute())
+        self.playlist_video = PlayList.youtube.playlists().list(id=playlist_id, part='snippet',
+                                                                 maxResults=50, ).execute()
+
         self.video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist_videos['items']]
         self.video_response = PlayList.youtube.videos().list(part='contentDetails,statistics',
                                                         id=','.join(self.video_ids)).execute()
         self.playlist_id = playlist_id
-        self.title = self.playlist_videos['items'][0]['snippet']['title']
+        self.title = self.playlist_video['items'][0]['snippet']['title']
         self.url = f"https://www.youtube.com/playlist?list={self.playlist_id}"
 
 
