0cbca2a3febe5473ecab116bf7f6abefb5a066ce
vladislav <vladisla.sv.299563@gmail.com>
2023-08-27 16:16:36 +0600

    add homework_6

--- src/video.py
+++ src/video.py
@@ -1,7 +1,10 @@
 import os
+import requests
 
 from googleapiclient.discovery import build
 
+class PLNotFound(Exception):
+    pass
 
 class Video:
     api_key: str = os.getenv('YT_API_KEY')
@@ -9,17 +12,27 @@ class Video:
 
     def __init__(self, id_video):
         self.id_video = id_video
-        video_response = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+        response = requests.get(f'https://www.youtube.com/watch?v={self.id_video}')
+        if response.status_code != 200:
+            raise PLNotFound
+
+        try:
+            self.video_response = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                      id=self.id_video).execute()
-        self.video_title: str = video_response['items'][0]['snippet']['title']
-        self.url = f"https://www.youtube.com/watch?v={id_video}"
-        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
-        self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
+            self.title: str = self.video_response['items'][0]['snippet']['title']
+            self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+            self.comment_count: int = self.video_response['items'][0]['statistics']['commentCount']
+        except IndexError:
+            self.title = None
+            self.view_count = None
+            self.like_count = None
+            self.comment_count = None
+            self.url = f'https://www.youtube.com/watch?v={self.id_video}'
+
 
     def __str__(self):
-        return self.video_title
-
+        return self.title
 
 class PLVideo(Video):
     def __init__(self, id_video, playlist_id):
@@ -30,3 +43,11 @@ class PLVideo(Video):
         playlist_videos = Video.youtube.playlistItems().list(playlistId=self.playlist_id, part='snippet',
                                                              maxResults=50, ).execute()
         return playlist_videos['items'][0]['snippet']['title']
+
+
+def send_request(site_name):
+    response = requests.get(site_name)
+    if response.status_code != 200:
+        return None
+
+
